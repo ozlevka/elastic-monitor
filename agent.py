e8dcfd8d9fd41964cde697c47ef649a751f366cf
Levka <ozlevka@gmail.com>
2016-07-15 15:14:49 +0300

    Raw data is indexed to elastic

--- agent.py
+++ agent.py
@@ -2,18 +2,16 @@
 import sys
 import yaml
 from elasticsearch import Elasticsearch
+import psutil
+from datetime import datetime
+import socket
+import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
-
-
-class Elastic:
-    def __init__(self, hosts):
-        self.client = Elasticsearch(hosts)
-
 
 
 class SimpleReader(object):
     '''
-    Contains base types for Elasticsearch communictaion
+    Contains base types for Elasticsearch comunictaion
     '''
     def __init__(self, config):
         self.target = Elasticsearch(config.get_elastic_target_host())
@@ -29,17 +27,85 @@ class ElasticStatsReader(SimpleReader):
 class SystemStatsReader(SimpleReader):
     def __init__(self, config):
         super(SystemStatsReader, self).__init__(config)
+        self.config = config
 
+    def read_data(self):
+        cpu = psutil.cpu_times()
+        vm = psutil.virtual_memory()
+        swm = psutil.swap_memory()
+        dio = psutil.disk_io_counters()
+        nio = psutil.net_io_counters(pernic=False)
 
+        return {
+            'host': socket.gethostname(),
+            '@timestamp': datetime.now(),
+            'cpu': {
+                'user': cpu.user,
+                'nice': cpu.nice,
+                'system': cpu.system,
+                'idle': cpu.idle,
+                'iowait': cpu.iowait,
+                'irq': cpu.irq,
+                'softirq': cpu.softirq,
+                'steal': cpu.steal,
+                'guest': cpu.guest,
+                'guest_nice': cpu.guest_nice
+            },
+            'mem': {
+                'virtual': {
+                    'total': vm.total,
+                    'available': vm.available,
+                    'percent': vm.percent,
+                    'used': vm.used,
+                    'free': vm.free,
+                    'active': vm.active,
+                    'inactive': vm.inactive,
+                    'buffers': vm.buffers,
+                    'cached': vm.cached,
+                    'shared': vm.shared
+                },
+                'swap': {
+                    'total': swm.total,
+                    'used': swm.used,
+                    'free': swm.free,
+                    'percent': swm.percent,
+                    'sin': swm.sin,
+                    'sout': swm.sout
+                }
+            },
+            'diskio': {
+                'read_count': dio.read_count,
+                'write_count': dio.write_count,
+                'read_bytes': dio.read_bytes,
+                'write_bytes': dio.write_bytes,
+                'read_time': dio.read_time,
+                'write_time': dio.write_time,
+                'read_merged_count': dio.read_merged_count,
+                'write_merged_count': dio.write_merged_count,
+                'busy_time': dio.busy_time
+            },
+            "netio": {
+                'bytes_sent': nio.bytes_sent,
+                'bytes_recv': nio.bytes_recv,
+                'packets_sent': nio.packets_sent,
+                'packets_recv': nio.packets_recv,
+                'errin': nio.errin,
+                'errout': nio.errout,
+                'dropin': nio.dropin,
+                'dropout': nio.dropout
+            }
+        }
 
+    def index_data(self):
+        data = self.read_data()
 
-class ElasticWriter:
-    def __init__(self):
-        self.elastic = Elastic
-
+        return self.target.index(self.config.get_target_index_name(), self.config.get_target_system_type(), data)
 
 
 class Configuration:
+    '''
+    Contains information reader from config yaml
+    '''
     def __init__(self, config):
         self.config = config
 
@@ -52,21 +118,40 @@ class Configuration:
     def get_elastic_source_host(self):
         return self.config['elasticsearch']['source']['host'] + ':' + str(self.config['elasticsearch']['source']['port'])
 
+    def get_target_index_name(self):
+        return self.config['elasticsearch']['target']['index']
 
-def run():
-    pass
+    def get_target_system_type(self):
+        return  self.config['elasticsearch']['target']['types']['system']
 
+
+
+def run_system_only():
+    global conf
+    system = SystemStatsReader(config=conf)
+    result = system.index_data()
+    print
 
 
 def main(args):
-    f = open('./config/config.yaml')
-    conf = Configuration(yaml.load(f))
-    f.close()
-
-
-
+    run_flag = args[1]
+    bs = BlockingScheduler()
+    interval = conf.get_interval()
+    bs.add_executor('processpool')
+    if run_flag == 'sys':
+        bs.add_job(run_system_only, 'interval', seconds=interval['value'])
+    elif run_flag == 'elastic':
+        pass
+    else:
+        pass
+    bs.start()
 
 
 
 if __name__ == '__main__':
+    f = open('./config/config.yaml')
+    conf = Configuration(yaml.load(f))
+    f.close()
+    logging.basicConfig()
+    logging.getLogger('apscheduler.scheduler').setLevel(logging.DEBUG)
     main(sys.argv)
--- tests/tests.py
+++ tests/tests.py
@@ -7,18 +7,22 @@ import yaml
 from agent import Configuration
 from agent import ElasticStatsReader
 from agent import SystemStatsReader
+import socket
 
 
 def configuration_test():
     config = {
         'scheduler': {
-            'interval': '10s'
+            'interval': {
+                "units": 'seconds',
+                'value': 10
+            }
         }
     }
 
     c = Configuration(config)
 
-    print 'Get interval test ' +  str(c.get_interval() == '10s')
+    print 'Get interval test ' +  str(c.get_interval()['units'] == 'seconds' and c.get_interval()['value'] == 10)
 
 
 def test_data_structures():
@@ -33,6 +37,23 @@ def test_data_structures():
     print estats.target.indices.stats()
 
 
+def test_system_data():
+    f = open('../config/config.yaml')
+    c = Configuration(yaml.load(f))
+    f.close()
+    sysreader = SystemStatsReader(c)
+    print sysreader.read_data()
+
+
+def test_index_system_data():
+    f = open('../config/config.yaml')
+    c = Configuration(yaml.load(f))
+    f.close()
+    sysreader = SystemStatsReader(c)
+    print sysreader.index_data()
+
 if __name__ == '__main__':
     #configuration_test()
-    test_data_structures()
+    #test_data_structures()
+    #test_system_data()
+    test_index_system_data()
